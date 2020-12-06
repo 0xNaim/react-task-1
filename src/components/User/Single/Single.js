fc9fdc86595b24ce78a7f4212fd12c89df59e480
“[Naim <[naimislam3545@gmail.com]>
2020-12-06 13:00:18 +0600

    React Task Complete

---
+++ src/components/User/Single/Single.js
@@ -0,0 +1,29 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./Single.css";
+
+const Single = (props) => {
+  const { name, email, phone, address } = props.users;
+  const handleCount = props.handleCount;
+  return (
+    <div className="single-user">
+      <img
+        src="https://images.unsplash.com/photo-1565464027194-7957a2295fb7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fG1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        alt=""
+      />
+      <div className="user-info">
+        <h2>Name: {name}</h2>
+        <h4>Email: {email}</h4>
+        <h4>Phone: {phone}</h4>
+        <h4>Address: {address.street}</h4>
+        <button onClick={() => handleCount(props.users)}>
+          <FontAwesomeIcon icon={faPlus} /> Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Single;
+<h1>I am from Single</h1>;
---
+++ src/components/User/User.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import Single from "./Single/Single";
+import "./User.css";
+
+const User = () => {
+  const [user, setUser] = useState([]);
+  const [count, setCount] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
+  }, []);
+  const handleCount = (users) => {
+    const newCount = [...count, users];
+    setCount(newCount)
+  };
+  return (
+    <div className="user-container">
+      <div className="user-content">
+        ]
+        {user.map((user) => (
+          <Single
+            users={user}
+            handleCount={handleCount}
+            key={user.id}
+          ></Single>
+        ))}
+      </div>
+      <div className="user-count">
+        <h2>Friend Added: {count.length}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default User;
